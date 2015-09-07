131d4f6737a19282d9dd1051e3b4be58ac177ee7
Doug Torrance <dtorrance@piedmont.edu>
2015-09-06 23:39:40 -0400

    check-package.sh - new script; check and tag recently uploaded package

---
+++ check-package.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+PACKAGE=$1
+cd ~/src/$PACKAGE/$PACKAGE
+dpkg-source -b .
+VERSION=`dpkg-parsechangelog -S Version`
+FIRSTLETTER=`echo $PACKAGE | cut -c1`
+cd ~/tmp
+dget http://incoming.debian.org/debian-buildd/pool/main/${FIRSTLETTER}/${PACKAGE}/${PACKAGE}_${VERSION}.dsc
+debdiff ~/src/$PACKAGE/${PACKAGE}_${VERSION}.dsc ${PACKAGE}_${VERSION}.dsc
+echo -n "continue? (y/n) "
+read ANSWER
+if echo "$ANSWER" | grep -iq "^y"
+then
+    cd ~/src/$PACKAGE/$PACKAGE
+    TAG=`echo $VERSION | sed 's/~/_/'`
+    git tag debian/$TAG
+    git push origin debian/$TAG
+fi
